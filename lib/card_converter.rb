6aa99897ab3a47842dd9c9b328863dc671a1fa04
Joe Webster <joseph.scott.webster@gmail.com>
2022-02-15 17:34:45 -0500

    Add flash card CSV file and create CardConverter class

---
+++ lib/card_converter.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'csv'
+require_relative 'card'
+
+class CardConverter
+  def self.create_cards
+    path = 'data/cards.csv'
+    table = CSV.read(path, headers: true)
+
+    table.each_with_object([]) do |row, cards|
+      question = row[0]
+      answer = row['answer']
+      category = row['category']
+
+      cards << Card.new(question, answer, category)
+    end
+  end
+end
