5f3e013c91ee7b74eb44e243538bb32efb2a0d22
Joe Webster <joseph.scott.webster@gmail.com>
2022-02-15 17:37:49 -0500

    First pass at loading cards from csv file

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative 'lib/card'
+require_relative 'lib/card_converter'
 require_relative 'lib/deck'
 require_relative 'lib/turn'
 require_relative 'lib/round'
@@ -15,10 +15,7 @@ class FlashCardSession
   private
 
   def initialize
-    card1 = Card.new('What is the capital of Alaska?', 'Juneau', :Geography)
-    card2 = Card.new('The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?', 'Mars', :STEM)
-    card3 = Card.new('Describe in words the exact direction that is 697.5° clockwise from due north?', 'North north west', :STEM)
-    cards = [card1, card2, card3]
+    cards = CardConverter.create_cards
     deck = Deck.new(cards)
     @round = Round.new(deck)
   end
